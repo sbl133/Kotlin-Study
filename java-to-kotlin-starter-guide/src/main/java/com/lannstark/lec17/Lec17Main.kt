1f57dcfc58a06ab501b6f403db7704ae607d0090
Bum <sbl133@naver.com>
2022-10-16 23:54:17 +0900

    [feat] lambda in kotlin

--- java-to-kotlin-starter-guide/src/main/java/com/lannstark/lec17/Lec17Main.kt
+++ java-to-kotlin-starter-guide/src/main/java/com/lannstark/lec17/Lec17Main.kt
@@ -1,4 +1,23 @@
 package com.lannstark.lec17
 
-class Lec17Main {
+fun main() {
+    val fruits = listOf(
+        Fruit("사과", 1000),
+        Fruit("사과", 1200),
+        Fruit("사과", 1200),
+        Fruit("사과", 1500),
+        Fruit("바나나", 3000),
+        Fruit("바나나", 3200),
+        Fruit("바나나", 2500),
+        Fruit("수박", 10000),
+    )
+
+    val isApple = fun(fruit: Fruit): Boolean {
+        return fruit.name == "사과"
+    }
+
+    val isApple2 = {fruit: Fruit -> fruit.name == "사과"}
+
+    isApple(fruits[0])
+    isApple2.invoke(fruits[0])
 }
